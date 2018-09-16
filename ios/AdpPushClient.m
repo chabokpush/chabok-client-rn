6b7822a508f92ecf6ffe0ff6199fd0f44ffd475d
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2018-09-16 18:50:54 +0430

    Fix bug publish method in iOS bridge

--- ios/AdpPushClient.m
+++ ios/AdpPushClient.m
@@ -38,98 +38,98 @@ RCT_EXPORT_METHOD(init:(NSString *) appId
                                                                 apiKey:apiKey
                                                               userName:username
                                                               password:password];
-  if (state) {
-    RCTLogInfo(@"Initilized sucessfully");
-    resolve(@{@"result":@"Initilized sucessfully"});
-  } else {
-    RCTLogInfo(@"Could not init chabok parameters");
-    NSError *error = [[NSError alloc] initWithDomain:NSLocalizedDescriptionKey
-                                                code:400
-                                            userInfo:@{
-                                                       @"result":@"Could not init chabok parameters"
-                                                       }];
-    reject(@"400",@"Could not init chabok parameters",error);
-  }
+    if (state) {
+        RCTLogInfo(@"Initilized sucessfully");
+        resolve(@{@"result":@"Initilized sucessfully"});
+    } else {
+        RCTLogInfo(@"Could not init chabok parameters");
+        NSError *error = [[NSError alloc] initWithDomain:NSLocalizedDescriptionKey
+                                                    code:400
+                                                userInfo:@{
+                                                           @"result":@"Could not init chabok parameters"
+                                                           }];
+        reject(@"400",@"Could not init chabok parameters",error);
+    }
 }
 
 RCT_EXPORT_METHOD(initializeApp:(NSString *) appName options:(NSDictionary *) options cbk:(RCTResponseSenderBlock) cbk) {
-
-  if(options == nil || [options isEqual:[NSNull null]]){
-    RCTLogInfo(@"Option parameter is null");
-    cbk(@[@{@"result":@"Option parameter is null"}]);
-  } else {
     
-    BOOL devMode = [[options valueForKey:@"isDev"] boolValue];
-    
-    [PushClientManager setDevelopment:devMode];
-    PushClientManager.defaultManager.enableLog = YES;
-    [PushClientManager.defaultManager addDelegate:self];
-    
-    NSString *appId = [options valueForKey:@"appId"];
-    NSString *apiKey = [options valueForKey:@"apiKey"];
-    NSString *username = [options valueForKey:@"username"];
-    NSString *password = [options valueForKey:@"password"];
-    
-    NSArray *appIds = [appId componentsSeparatedByString:@"/"];
-    [self init:appIds.firstObject
-             apiKey:apiKey
-           username:username
-           password:password
-       promise:^(id result) {
-         cbk(result);
-       } rejecter:^(NSString *code, NSString *message, NSError *error) {
-         cbk(@[@{@"error":message}]);
-       }];
-  }
+    if(options == nil || [options isEqual:[NSNull null]]){
+        RCTLogInfo(@"Option parameter is null");
+        cbk(@[@{@"result":@"Option parameter is null"}]);
+    } else {
+        
+        BOOL devMode = [[options valueForKey:@"isDev"] boolValue];
+        
+        [PushClientManager setDevelopment:devMode];
+        PushClientManager.defaultManager.enableLog = YES;
+        [PushClientManager.defaultManager addDelegate:self];
+        
+        NSString *appId = [options valueForKey:@"appId"];
+        NSString *apiKey = [options valueForKey:@"apiKey"];
+        NSString *username = [options valueForKey:@"username"];
+        NSString *password = [options valueForKey:@"password"];
+        
+        NSArray *appIds = [appId componentsSeparatedByString:@"/"];
+        [self init:appIds.firstObject
+            apiKey:apiKey
+          username:username
+          password:password
+           promise:^(id result) {
+               cbk(result);
+           } rejecter:^(NSString *code, NSString *message, NSError *error) {
+               cbk(@[@{@"error":message}]);
+           }];
+    }
 }
 
 #pragma mark - Register methods
 
 RCT_EXPORT_METHOD(register:(NSString *)userId) {
-  if (userId && ![userId isEqual:[NSNull null]]){
-    BOOL state = [PushClientManager.defaultManager registerUser:userId];
-    if (state) {
-      RCTLogInfo(@"Registered to chabok");
+    if (userId && ![userId isEqual:[NSNull null]]){
+        BOOL state = [PushClientManager.defaultManager registerUser:userId];
+        if (state) {
+            RCTLogInfo(@"Registered to chabok");
+        } else {
+            RCTLogInfo(@"Fail to registered to chabok");
+        }
     } else {
-      RCTLogInfo(@"Fail to registered to chabok");
+        RCTLogInfo(@"Could not register userId to chabok");
     }
-  } else {
-    RCTLogInfo(@"Could not register userId to chabok");
-  }
 }
 
 RCT_EXPORT_METHOD(register:(NSString *)userId channel:(NSString *) channel) {
-  if (userId && ![userId isEqual:[NSNull null]]){
-    
-    BOOL state = [PushClientManager.defaultManager registerUser:userId
-                                                      channels:@[channel]];
-    if (state) {
-      RCTLogInfo(@"Registered to chabok with channel");
+    if (userId && ![userId isEqual:[NSNull null]]){
+        
+        BOOL state = [PushClientManager.defaultManager registerUser:userId
+                                                           channels:@[channel]];
+        if (state) {
+            RCTLogInfo(@"Registered to chabok with channel");
+        } else {
+            RCTLogInfo(@"Fail to registered to chabok");
+        }
     } else {
-      RCTLogInfo(@"Fail to registered to chabok");
+        RCTLogInfo(@"Could not register userId to chabok with channel");
     }
-  } else {
-    RCTLogInfo(@"Could not register userId to chabok with channel");
-  }
 }
 
 RCT_EXPORT_METHOD(register:(NSString *)userId channels:(NSArray *) channels) {
-  if (userId && ![userId isEqual:[NSNull null]]){
-    
-    NSArray *chnl = @[];
-    if (![channels isEqual:[NSNull null]] && channels) {
-      chnl = channels;
-    }
-    BOOL state = [PushClientManager.defaultManager registerUser:userId
-                                                      channels:chnl];
-    if (state) {
-      RCTLogInfo(@"@@@@@@@@@@@@@@@@@ Registered to chabok with channels");
+    if (userId && ![userId isEqual:[NSNull null]]){
+        
+        NSArray *chnl = @[];
+        if (![channels isEqual:[NSNull null]] && channels) {
+            chnl = channels;
+        }
+        BOOL state = [PushClientManager.defaultManager registerUser:userId
+                                                           channels:chnl];
+        if (state) {
+            RCTLogInfo(@"@@@@@@@@@@@@@@@@@ Registered to chabok with channels");
+        } else {
+            RCTLogInfo(@"@@@@@@@@@@@@@@@@@ Fail to registered to chabok");
+        }
     } else {
-      RCTLogInfo(@"@@@@@@@@@@@@@@@@@ Fail to registered to chabok");
+        RCTLogInfo(@"@@@@@@@@@@@@@@@@@ Could not register userId to chabok with channels");
     }
-  } else {
-    RCTLogInfo(@"@@@@@@@@@@@@@@@@@ Could not register userId to chabok with channels");
-  }
 }
 
 RCT_EXPORT_METHOD(unregister) {
@@ -149,7 +149,7 @@ RCT_EXPORT_METHOD(getUserId:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromise
 #pragma mark - dev
 
 RCT_EXPORT_METHOD(setDevelopment:(BOOL) devMode) {
-  [PushClientManager setDevelopment:devMode];
+    [PushClientManager setDevelopment:devMode];
 }
 
 #pragma mark - tags
@@ -162,7 +162,7 @@ RCT_EXPORT_METHOD(addTag:(NSString *) tagName resolver:(RCTPromiseResolveBlock)r
                                      } failure:^(NSError *error) {
                                          NSString *errorCode = [NSString stringWithFormat:@"%zd",error.code];
                                          reject(errorCode,error.domain,error);
-                                   }];
+                                     }];
 }
 RCT_EXPORT_METHOD(addTags:(NSArray *) tagsName resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
@@ -172,7 +172,7 @@ RCT_EXPORT_METHOD(addTags:(NSArray *) tagsName resolver:(RCTPromiseResolveBlock)
                                       } failure:^(NSError *error) {
                                           NSString *errorCode = [NSString stringWithFormat:@"%zd",error.code];
                                           reject(errorCode,error.domain,error);
-                                   }];
+                                      }];
 }
 RCT_EXPORT_METHOD(removeTag:(NSString *) tagName resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
@@ -182,7 +182,7 @@ RCT_EXPORT_METHOD(removeTag:(NSString *) tagName resolver:(RCTPromiseResolveBloc
                                         } failure:^(NSError *error) {
                                             NSString *errorCode = [NSString stringWithFormat:@"%zd",error.code];
                                             reject(errorCode,error.domain,error);
-                                   }];
+                                        }];
 }
 RCT_EXPORT_METHOD(removeTags:(NSArray *) tagsName resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
@@ -192,81 +192,91 @@ RCT_EXPORT_METHOD(removeTags:(NSArray *) tagsName resolver:(RCTPromiseResolveBlo
                                          } failure:^(NSError *error) {
                                              NSString *errorCode = [NSString stringWithFormat:@"%zd",error.code];
                                              reject(errorCode,error.domain,error);
-                                      }];
+                                         }];
 }
 
 #pragma mark - publish
 
-RCT_EXPORT_METHOD(publish:(NSString *) channel text:(NSString *) text resolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject) {
-  BOOL publishState = [PushClientManager.defaultManager publish:channel withText:text];
-  resolve(@[@{@"published":@(publishState)}]);
-}
-
-RCT_EXPORT_METHOD(publish:(NSString *) userId channel:(NSString *) channel text:(NSString *) text resolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject) {
-  BOOL publishState = [PushClientManager.defaultManager publish:userId toChannel:channel withText:text];
-  resolve(@[@{@"published":@(publishState)}]);
-}
+//RCT_EXPORT_METHOD(publish:(NSString *) channel text:(NSString *) text resolver:(RCTPromiseResolveBlock)resolve
+//                  rejecter:(RCTPromiseRejectBlock)reject) {
+//  BOOL publishState = [PushClientManager.defaultManager publish:channel withText:text];
+//  resolve(@[@{@"published":@(publishState)}]);
+//}
+//
+//RCT_EXPORT_METHOD(publish:(NSString *) userId channel:(NSString *) channel text:(NSString *) text resolver:(RCTPromiseResolveBlock)resolve
+//                  rejecter:(RCTPromiseRejectBlock)reject) {
+//  BOOL publishState = [PushClientManager.defaultManager publish:userId toChannel:channel withText:text];
+//  resolve(@[@{@"published":@(publishState)}]);
+//}
 
 RCT_EXPORT_METHOD(publish:(NSDictionary *) message resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
-  PushClientMessage *chabokMessage = [[PushClientMessage alloc] initWithJson:message
-                                                               channel:[message valueForKey:@"channel"]];
-  BOOL publishState = [PushClientManager.defaultManager publish:chabokMessage];
-  resolve(@[@{@"published":@(publishState)}]);
+    NSDictionary *data = [message valueForKey:@"data"];
+    NSString *userId = [message valueForKey:@"userId"];
+    NSString *content = [message valueForKey:@"content"];
+    NSString *channel = [message valueForKey:@"channel"];
+    
+    PushClientMessage *chabokMessage;
+    if (data) {
+        chabokMessage = [[PushClientMessage alloc] initWithMessage:content withData:data toUserId:userId channel:channel];
+    } else {
+        chabokMessage = [[PushClientMessage alloc] initWithMessage:content toUserId:userId channel:channel];
+    }
+    
+    BOOL publishState = [PushClientManager.defaultManager publish:chabokMessage];
+    resolve(@{@"published":@(publishState)});
 }
 
 #pragma mark - subscribe
 RCT_EXPORT_METHOD(subscribe:(NSString *) channel) {
-  [PushClientManager.defaultManager subscribe:channel];
+    [PushClientManager.defaultManager subscribe:channel];
 }
 
 #pragma mark - unsubscribe
 RCT_EXPORT_METHOD(unSubscribe:(NSString *) channel) {
-  [PushClientManager.defaultManager unsubscribe:channel];
+    [PushClientManager.defaultManager unsubscribe:channel];
 }
 
 #pragma mark - badge
 RCT_EXPORT_METHOD(resetBadge) {
-  [PushClientManager resetBadge];
+    [PushClientManager resetBadge];
 }
 
 #pragma mark - track
 RCT_EXPORT_METHOD(track:(NSString *) trackName data:(NSDictionary *) data) {
-  [PushClientManager.defaultManager track:trackName data:data];
+    [PushClientManager.defaultManager track:trackName data:data];
 }
 
 #pragma mark - chabok delegate methods
 - (NSArray<NSString *> *)supportedEvents{
-  return @[@"connectionStatus",@"ChabokMessageReceived"];
+    return @[@"connectionStatus",@"ChabokMessageReceived"];
 }
 
 -(void) pushClientManagerDidReceivedMessage:(PushClientMessage *)message{
-  if (self.bridge) {
-    [self sendEventWithName:@"ChabokMessageReceived" body:[message toDict]];
-  }
+    if (self.bridge) {
+        [self sendEventWithName:@"ChabokMessageReceived" body:[message toDict]];
+    }
 }
 
 -(void) pushClientManagerDidChangedServerConnectionState {
-  NSString *connectionState = @"";
-  if (PushClientManager.defaultManager.connectionState == PushClientServerConnectedState) {
-    connectionState = @"CONNECTED";
-  } else if (PushClientManager.defaultManager.connectionState == PushClientServerConnectingState ||
-             PushClientManager.defaultManager.connectionState == PushClientServerConnectingStartState) {
-    connectionState = @"CONNECTING";
-  } else if (PushClientManager.defaultManager.connectionState == PushClientServerDisconnectedState ||
-             PushClientManager.defaultManager.connectionState == PushClientServerDisconnectedErrorState) {
-    connectionState = @"DISCONNECTED";
-  } else {
-    connectionState = @"NOT_INITIALIZED";
-  }
-
-  [self sendEventWithName:@"connectionStatus" body:connectionState];
+    NSString *connectionState = @"";
+    if (PushClientManager.defaultManager.connectionState == PushClientServerConnectedState) {
+        connectionState = @"CONNECTED";
+    } else if (PushClientManager.defaultManager.connectionState == PushClientServerConnectingState ||
+               PushClientManager.defaultManager.connectionState == PushClientServerConnectingStartState) {
+        connectionState = @"CONNECTING";
+    } else if (PushClientManager.defaultManager.connectionState == PushClientServerDisconnectedState ||
+               PushClientManager.defaultManager.connectionState == PushClientServerDisconnectedErrorState) {
+        connectionState = @"DISCONNECTED";
+    } else {
+        connectionState = @"NOT_INITIALIZED";
+    }
+    
+    [self sendEventWithName:@"connectionStatus" body:connectionState];
 }
 
 - (void)invalidate {
-  self.appId = nil;
+    self.appId = nil;
 }
 
 @end
