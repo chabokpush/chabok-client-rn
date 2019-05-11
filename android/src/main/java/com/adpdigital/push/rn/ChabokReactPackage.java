c1ae79fa0ca33920f7fa66b4362a4c0cccfc9c39
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2019-05-11 09:51:00 +0430

    Add createJSModules method to package for support RN 47

--- android/src/main/java/com/adpdigital/push/rn/ChabokReactPackage.java
+++ android/src/main/java/com/adpdigital/push/rn/ChabokReactPackage.java
@@ -33,6 +33,11 @@ public class ChabokReactPackage implements ReactPackage {
         return modules;
     }
 
+    // Deprecated RN 0.47
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
